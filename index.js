27280344b68987b95978d56f68b83c06306148ca
François-Joseph du Fou <francoisjoseph.du.fou@gmail.com>
2020-04-16 22:48:53 +0200

    2

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function setPresence(bot) {
       .then(() => console.log('activity refresh'))
       .catch(console.log)
   } else {
-    bot.user.setPresence({ game: { name: `.help | Use by ${getCountUsers()} users.`, type: 'PLAYING' }, status: 'online' }) // Change to "do not distibute" for Christmas.
+    bot.user.setPresence({ game: { name: `.help | Use by ${getCountUsers()} users in ${bot.guilds.size} guilds.`, type: 'PLAYING' }, status: 'online' }) // Change to "do not distibute" for Christmas.
       .then(() => console.log('activity refresh'))
       .catch(console.log)
   }
@@ -315,21 +315,47 @@ bot.on('message', message => {
       return message.channel.send("You cant delete messages!");
     let args = message.content.split(" ").slice(1);
     if (!args[0]) return message.channel.send("Number of messages please!")
-    if (args > 100) {
-      message.channel.send("100 is the maximum.")
+    if (args == "all") {
+      message.channel.fetchMessages()
+        .then(function (list) {
+          message.channel.bulkDelete(list);
+          let reportEmbed = new Discord.RichEmbed()
+            .setTitle("Clear:", message.guild.iconURL)
+            .setDescription(" ")
+            .setColor(color)
+            .addField(`All messages has succesfull deleted`, `by ${message.author}`)
+            .setFooter(nameb + vo)
+            .setTimestamp()
+          //.addField("Reason", `${args[1]}`);
+          message.channel.send(reportEmbed)
+        }, function (err) { message.channel.send("ERROR: ERROR CLEARING CHANNEL.") })
     } else {
-      message.delete().catch(O_o => { });
-      message.channel.bulkDelete(args[0]).then(() => {
-        let reportEmbed = new Discord.RichEmbed()
-          .setTitle("Clear:", message.guild.iconURL)
-          .setDescription(" ")
-          .setColor(color)
-          .addField(`${args[0]} messages has succesfull deleted`, `by ${message.author}`)
-          .setFooter(nameb + vo)
-          .setTimestamp()
-        //.addField("Reason", `${args[1]}`);
-        message.channel.send(reportEmbed)
-      })
+      if (isNaN(args)) return message.channel.send('The amount parameter is not a number!'); // Checks if the `amount` parameter is a number. If not, the command throws an error
+      if (args > 100) {
+        message.channel.send("100 is the maximum or use tje \"all\".")
+      } else {
+        message.delete().catch(O_o => { });
+        message.channel.bulkDelete(args[0]).then(() => {
+          let reportEmbed = new Discord.RichEmbed()
+            .setTitle("Clear:", message.guild.iconURL)
+            .setDescription(" ")
+            .setColor(color)
+            .addField(`${args[0]} messages has succesfull deleted`, `by ${message.author}`)
+            .setFooter(nameb + vo)
+            .setTimestamp()
+          //.addField("Reason", `${args[1]}`);
+          message.channel.send(reportEmbed)
+        })
+      }
+    }
+  }
+
+  if (message.content === prefix + 'delete') {
+    if (message.member.hasPermission("MANAGE_MESSAGES")) {
+      message.channel.fetchMessages()
+        .then(function (list) {
+          message.channel.bulkDelete(list);
+        }, function (err) { message.channel.send("ERROR: ERROR CLEARING CHANNEL.") })
     }
   }
 
@@ -505,12 +531,15 @@ bot.on('message', message => {
       .setDescription(" ")
       .setColor(color)
       .addField("Introduction:", "Azure by O3IL.")
-      .addField("Create the:", `${moment(bot.user.createdAt).format("DD/MM/YYYY hh:mm")}`, true)
+      .addField("Servers: ", `${bot.guilds.size.toLocaleString()}`, true)
+      .addField("Saloons: ", `${bot.channels.size}`, true)
       .addField("Users: ", `${getCountUsers()}`, true)
+      .addField("API latency: ", `${Math.round(bot.ping)}ms`, true)
       .addField("Memory: ", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
       .addField("Online time: ", `${getUptime()}`, true)
       .addField("Discord.js: ", `${version}`, true)
       .addField("Azure:", vo, true)
+      .addField("Create the:", `${moment(bot.user.createdAt).format("DD/MM/YYYY hh:mm")}`, true)
       .setFooter(nameb + vo)
       .setThumbnail(bot.user.displayAvatarURL)
       .setTimestamp();
